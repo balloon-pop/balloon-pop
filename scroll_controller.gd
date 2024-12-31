0d0e7f6fd3ae9e6c6dd08cff0a6222486adccf56
ChanhuiSeok <chanhuicom@gmail.com>
2024-12-31 10:11:00 +0900

    배경 및 임시 캐릭터 애셋 추가, 메인 씬에 parallax2D 추가

---
+++ scroll_controller.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var speed := 500
+var direction: float
+var direction2: float
+
+func _physics_process(_delta):
+	direction = Input.get_axis("ui_up", "ui_down")
+
+	# 좌우 테스트
+	# direction2 = Input.get_axis("ui_left", "ui_right")
+	# if direction2:
+	# 	velocity.x = direction2 * speed
+	# else:
+	# 	velocity.x = move_toward(velocity.x, 0, speed)
+	
+	if direction:
+		velocity.y = direction * speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed)
+	
+	move_and_slide()
