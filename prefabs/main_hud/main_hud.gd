1352fa450c232062dcab57c91b691cd2144573af
ChanhuiSeok <chanhuicom@gmail.com>
2025-01-04 02:44:22 +0900

    main_hud에 고도 몇 m인지 표시하는 부분 추가

--- prefabs/main_hud/main_hud.gd
+++ prefabs/main_hud/main_hud.gd
@@ -3,14 +3,18 @@ extends CanvasLayer
 
 @onready var air_label: Label = $Control/AirLabel
 @onready var wind_texture: TextureProgressBar = $Control/TextureRect2
+@onready var altitude_label: Label = $Control/AltitudeLabel
 
 func _ready() -> void:
 	PlayerManager.on_air_count_change.connect(_on_air_count_change)
+	PlayerManager.player_altitude_change.connect(_on_player_altitude_change)
 
 func _process(_delta):
 	var dash_cool_percent = 1 - PlayerManager.dash_timer.time_left / PlayerManager.dash_timer.wait_time
 	wind_texture.value = wind_texture.max_value * dash_cool_percent
 
+func _on_player_altitude_change(altitude: int) -> void:
+	altitude_label.text = "%dm" % altitude
 
 func _on_air_count_change(count: int) -> void:
 	air_label.text = "%02d" % count
