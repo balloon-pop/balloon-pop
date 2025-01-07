da0b5034a0d610e67c148aaf2779be58957d122f
dooyeong20 <noviced22@gmail.com>
2025-01-07 20:46:05 +0900

    feat: 게임 시작 전 플레이어 이동 제어

--- prefabs/player/player.gd
+++ prefabs/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var GRAVITY := 40
+@export var gravity := 10
 # 좌우 스피드
 @export var SPEED := 70
 @export var JUMP_SPEED := -200
@@ -9,6 +9,19 @@ const GRAVITY_ACCELERATION := 1.5
 const SPEED_ACCELERATION := 5
 const MAX_JUMP_SPEED := -350
 
+
+func _ready():
+	GameManager.game_state_change.connect(_on_game_state_change)
+
+func _on_game_state_change(new_state: GameManager.GameState):
+	match new_state:
+		GameManager.GameState.PLAYING:
+			velocity.y = JUMP_SPEED
+			if velocity.y < MAX_JUMP_SPEED:
+				velocity.y = MAX_JUMP_SPEED	
+			gravity = 40
+		GameManager.GameState.READY:
+			gravity = 10
 
 func air_jump():
 	if not PlayerManager.can_air_jump(): return
@@ -23,17 +36,18 @@ func air_jump():
 
 func _physics_process(_delta):
 	var x_direction = Input.get_axis("ui_left", "ui_right")
-	velocity.y = move_toward(velocity.y, GRAVITY, GRAVITY_ACCELERATION)
+	velocity.y = move_toward(velocity.y, gravity, GRAVITY_ACCELERATION)
 	
-	if x_direction:
-		velocity.x = move_toward(velocity.x, x_direction * SPEED, SPEED_ACCELERATION)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED_ACCELERATION)
+	if GameManager.game_state == GameManager.GameState.PLAYING:
+		if x_direction:
+			velocity.x = move_toward(velocity.x, x_direction * SPEED, SPEED_ACCELERATION)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED_ACCELERATION)
 
-	if Input.is_action_just_pressed("ui_accept"):
-		air_jump()
+		if Input.is_action_just_pressed("ui_accept"):
+			air_jump()
 
-	PlayerManager.player_position_change.emit(position)
-	PlayerManager.player_velocity_change.emit(velocity)
+		PlayerManager.player_position_change.emit(position)
+		PlayerManager.player_velocity_change.emit(velocity)
 	
 	move_and_slide()
